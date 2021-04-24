7eb92270c1e801c72da6daab7d6590ef01f09647
Chomosukeee <t1-du-31@hotmail.fr>
2021-04-24 18:43:19 +0200

    [+] Ajout d'un geocoder pour chercher sa ville et y aller directement dessus. [•] Passage temporaire de Mapbox à OpenStreetMap.

--- COVDM-FRONT/src/app/map/map.component.ts
+++ COVDM-FRONT/src/app/map/map.component.ts
@@ -1,6 +1,9 @@
 import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import {icon, LatLng, Marker} from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import 'leaflet-geosearch/dist/geosearch.css';
+import 'leaflet-geosearch/assets/css/leaflet.css';
 
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
@@ -25,7 +28,6 @@ Marker.prototype.options.icon = iconDefault;
 export class MapComponent implements AfterViewInit {
 
     private map;
-    readonly accessToken = 'pk.eyJ1IjoiZGVsaXJpb3VzIiwiYSI6ImNra3duNmlhNzA2dXUydmw2aTIzMDVlNHIifQ.gC3ae8QyA_ercwGZO_koMw';
 
     private initMap(): void {
         this.map = L.map('map', {
@@ -34,45 +36,53 @@ export class MapComponent implements AfterViewInit {
             zoom: 5
         });
         // tslint:disable-next-line:max-line-length
-        const mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=' + this.accessToken, {
-            attribution: '© <a href="https://www.mapbox.com/feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            tileSize: 512,
-            zoomOffset: -1
-        });
-        mapboxTiles.addTo(this.map);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+        }).addTo(this.map);
 
-        LCG.geocoder().addTo(this.map);
     }
 
-    constructor() {
-    }
-
-    getGeolocation(): Promise<any> {
+    private getGeolocation(): Promise<any> {
         // TSLint rale à cause du "shadow-name" de resolve mais ça marche, osef.
         return new Promise((resolve, reject) => {
             navigator.geolocation.watchPosition(
                 position => {
                     resolve({lat: position.coords.latitude, lng: position.coords.longitude});
-                },
-                err => {
-                    reject(err);
-                });
+                    },
+                err => {reject(err); });
         });
+    }
+
+    private initGeocoder(): void {
+        const provider = new OpenStreetMapProvider();
+        // @ts-ignore
+        const searchControl = new GeoSearchControl({
+            provider,
+            autoComplete: true,
+            autoCompleteDelay: 250,
+            style: 'bar',
+            autoClose: true,
+            keepResult: true
+        });
+        this.map.addControl(searchControl);
     }
 
     ngAfterViewInit(): void {
         this.initMap();
+        this.initGeocoder();
         // Cherche la localisation de l'utilisateur.
-        // Réorganiser un peu tout ça...
         this.getGeolocation().then(pos => {
             console.log(`Position: ${pos.lat} ${pos.lng}`);
+
             const latlng = new LatLng(pos.lat, pos.lng);
 
             L.marker(latlng).addTo(this.map);
             L.circle(latlng, {radius: 10000}).addTo(this.map);
+
             this.map.flyTo(latlng, 10, {
                 animate: true,
-                duration: 2
+                duration: 1.5
             });
         })
             .catch(err => console.warn(err.message));
