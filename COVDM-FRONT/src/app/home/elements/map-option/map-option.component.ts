f55ade2b668b82ca7d736e9c8da4f72acac72177
Romain SALVAN <romain.salvan@g-mail.fr>
2021-04-29 13:16:09 +0200

    [+] MapOption component

---
+++ COVDM-FRONT/src/app/home/elements/map-option/map-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-option',
+  templateUrl: './map-option.component.html',
+  styleUrls: ['./map-option.component.scss']
+})
+export class MapOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
